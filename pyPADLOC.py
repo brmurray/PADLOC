1d1861f1a541ead01192ef8c97dc702c0d835dd4
brmurray <brmurray@vt.edu>
2020-09-28 10:37:41 -0700

    First Commit

---
+++ pyPADLOC.py
@@ -0,0 +1,59 @@
+"""
+PyPADLOC, pyPADLOC, a tool for planning how the PADLOC units
+          will interact with the WEC controller.
+
+Contact: Bryan Murray, brmurray@mailbox.org
+
+Versions:
+    09/28/2020 - First commit (BRM)
+
+"""
+
+#from importlib import reload  
+
+#import serial
+import calendar, datetime
+
+def introduce():
+    print("""
+          pyPADLOC, a tool for planning how the PADLOC units
+          will interact with the WEC controller.
+          
+          For now, it is meant to be used through IPython or another
+          interactive Python shell
+          """)
+    
+def parse(input):
+    '''Parse the string received from MASTER PADLOC unit'''
+    
+    # $WERST = master requests status from WEC
+    if input==r'$WERST':
+        print("Got status request")
+        print("reply = $WETST {WecStatus 20 characters}\r\n")
+        
+    
+     # $WECMD = master passed a command from pier -> SHORE -> MASTER -> WEC
+    elif input==r'$WECMD':
+        print("Got command from pier (via SHORE to MASTER")
+        print("reply = none")
+        
+    
+     # $WERST = master requests status from WEC
+    elif input==r'$WERTM':
+        print("Got time request")
+        dt = datetime.datetime.utcnow()
+        posix = calendar.timegm(dt.timetuple())
+        
+        reply = dt.strftime("%Y%m%d%H%M%S") + "{WecStatus 20 characters}\r\n"
+        print(r"reply = " + reply)
+        
+    elif input==r'$LCDAT':
+        print('Got loadcell datagram')
+        
+    else:
+        print("Error: unrecognized input")
+        
+        
+def getStatusMsg():
+    '''Request status from higher level controller'''
+    
